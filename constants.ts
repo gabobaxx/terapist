86fbb3e7c18445d4fc5f00ede8421abacb3867b0
gabrielba15 <gabriely101@hotmail.com>
2023-04-07 19:17:57 -0400

    feat: new changes at constanst

--- constants.ts
+++ constants.ts
@@ -1,9 +1,10 @@
-// Copyright 2023 the Deno authors. All rights reserved. MIT license.
-export const SITE_NAME = "Your SaaS";
-export const SITE_DESCRIPTION = "Some details about your SaaS.";
+export const SITE_NAME =
+	'Terapist - Planificador y Controlador de Vacunación Infantil';
+export const SITE_DESCRIPTION =
+	'Sistema Web para la Planificación y Control de Vacunación Infantil.';
 
-export const AUTHENTICATED_REDIRECT_PATH = "/dashboard";
+export const AUTHENTICATED_REDIRECT_PATH = '/dashboard';
 
-export const STRIPE_PREMIUM_PLAN_PRICE_ID = "price_1MPiEkD6QJts4RaYcp1SevPe";
+export const STRIPE_PREMIUM_PLAN_PRICE_ID = 'price_1MPiEkD6QJts4RaYcp1SevPe';
 
 export const FREE_PLAN_TODOS_LIMIT = 5;
