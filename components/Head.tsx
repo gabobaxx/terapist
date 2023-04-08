735ba4820c963ec5334ccc3b190b8daa5e100b68
gabrielba15 <gabriely101@hotmail.com>
2023-04-07 23:24:29 -0400

    feat: new styles

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,18 +1,25 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import { Head as _Head } from "$fresh/runtime.ts";
-import { SITE_DESCRIPTION, SITE_NAME } from "@/constants.ts";
+import { asset, Head as _Head } from '$fresh/runtime.ts';
+import { SITE_DESCRIPTION, SITE_NAME } from '@/constants.ts';
 
 interface HeadProps {
-  title?: string;
-  description?: string;
+	title?: string;
+	description?: string;
 }
 
 export default function Head({ title, description }: HeadProps) {
-  return (
-    <_Head>
-      <title>{title ?? SITE_NAME}</title>
-      <link rel="icon" href="/favicon.ico" sizes="32x32" />
-      <meta name="description" content={description ?? SITE_DESCRIPTION} />
-    </_Head>
-  );
+	return (
+		<_Head>
+			<title>{title ?? SITE_NAME}</title>
+			<link rel="icon" href="/favicon.ico" sizes="32x32" />
+			<link rel="stylesheet" href={asset('styles/main.css')} />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap"
+				rel="stylesheet"
+			/>
+			<meta name="description" content={description ?? SITE_DESCRIPTION} />
+		</_Head>
+	);
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -29,8 +29,8 @@ function Heading(props: HeadingProps) {
 function Hero() {
 	return (
 		<div class="text-center px-8 py-16 max-w-7xl mx-auto text-white space-y-8 flex-1 flex flex-col justify-center mt-[-112px]">
-			<h1 class="font-bold text-3xl md:text-7xl">Bienvenido a Terapist.</h1>
-			<p class="text-xl">
+			<h1 class="font-bold text-3xl md:text-8xl">Bienvenido a Terapist.</h1>
+			<p class="text-md md:text-2xl">
 				Terapist es un sistema web para la planificación y control de vacunación
 				infantil.
 			</p>
@@ -38,7 +38,7 @@ function Hero() {
 				<LinkButton href="/signup">Probar</LinkButton>
 				<LinkButton
 					href="/blog"
-					class="!bg-white border-2 border-pink-700 text-pink-700 hover:border-black hover:text-black transition duration-300"
+					class="!bg-white border-2 border-blue-700 text-blue-700 hover:border-black hover:text-black transition duration-300"
 				>
 					¿Cómo funciona?
 				</LinkButton>
