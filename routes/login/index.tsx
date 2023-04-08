f4a77d1c5cd6df668fbe23a8ef161a4b68ed0730
gabrielba15 <gabriely101@hotmail.com>
2023-04-07 21:08:08 -0400

    feat: redirect to account if user is authenticated

--- routes/login/index.tsx
+++ routes/login/index.tsx
@@ -1,37 +1,50 @@
-// Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { PageProps } from "$fresh/server.ts";
-import Logo from "@/components/Logo.tsx";
-import Head from "@/components/Head.tsx";
-import AuthForm from "@/components/AuthForm.tsx";
-import Notice from "@/components/Notice.tsx";
-import OAuthLoginButton from "@/components/OAuthLoginButton.tsx";
-import { GitHub } from "@/components/Icons.tsx";
+import type { Handlers, PageProps } from '$fresh/server.ts';
+import Logo from '@/components/Logo.tsx';
+import Head from '@/components/Head.tsx';
+import AuthForm from '@/components/AuthForm.tsx';
+import Notice from '@/components/Notice.tsx';
+import OAuthLoginButton from '@/components/OAuthLoginButton.tsx';
+import { GitHub } from '@/components/Icons.tsx';
+
+export const handler: Handlers = {
+	GET(req, ctx) {
+		if (req.headers.get('cookie')?.includes('supabase-auth-token'))
+			return new Response(null, {
+				status: 302,
+				headers: {
+					location: '/dashboard/account',
+				},
+			});
+
+		return ctx.render({ ...ctx.state });
+	},
+};
 
 export default function LoginPage(props: PageProps) {
-  const errorMessage = props.url.searchParams.get("error");
+	const errorMessage = props.url.searchParams.get('error');
 
-  return (
-    <>
-      <Head title="Login" />
-      <div class="max-w-xs flex h-screen m-auto">
-        <div class="m-auto w-72">
-          <a href="/">
-            <Logo class="mb-8" />
-          </a>
-          {errorMessage === "Invalid login credentials" && (
-            <Notice>{errorMessage}</Notice>
-          )}
-          <AuthForm type="Login" />
-          <hr class="my-4" />
-          <OAuthLoginButton provider="github">
-            <GitHub class="inline mr-2 h-5 w-5 align-text-top" />{" "}
-            Login with GitHub
-          </OAuthLoginButton>
-          <div class="text-center text-gray-500 hover:text-black mt-8">
-            <a href="/signup">Don't have an account? Sign up</a>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head title="Login" />
+			<div class="max-w-xs flex h-screen m-auto">
+				<div class="m-auto w-72">
+					<a href="/">
+						<Logo class="mb-8" />
+					</a>
+					{errorMessage === 'Invalid login credentials' && (
+						<Notice>{errorMessage}</Notice>
+					)}
+					<AuthForm type="Login" />
+					<hr class="my-4" />
+					<OAuthLoginButton provider="github">
+						<GitHub class="inline mr-2 h-5 w-5 align-text-top" /> Login with
+						GitHub
+					</OAuthLoginButton>
+					<div class="text-center text-gray-500 hover:text-black mt-8">
+						<a href="/signup">Don't have an account? Sign up</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- routes/signup.tsx
+++ routes/signup.tsx
@@ -1,37 +1,52 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-import type { PageProps } from "$fresh/server.ts";
-import Head from "@/components/Head.tsx";
-import AuthForm from "@/components/AuthForm.tsx";
-import Notice from "@/components/Notice.tsx";
-import Logo from "@/components/Logo.tsx";
-import OAuthLoginButton from "@/components/OAuthLoginButton.tsx";
-import { GitHub } from "@/components/Icons.tsx";
+import Head from '@/components/Head.tsx';
+import AuthForm from '@/components/AuthForm.tsx';
+import Notice from '@/components/Notice.tsx';
+import Logo from '@/components/Logo.tsx';
+import OAuthLoginButton from '@/components/OAuthLoginButton.tsx';
+import { GitHub } from '@/components/Icons.tsx';
+
+import type { Handlers, PageProps } from '$fresh/server.ts';
+
+export const handler: Handlers = {
+	GET(req, ctx) {
+		if (req.headers.get('cookie')?.includes('supabase-auth-token'))
+			return new Response(null, {
+				status: 302,
+				headers: {
+					location: '/dashboard/account',
+				},
+			});
+
+		return ctx.render({ ...ctx.state });
+	},
+};
 
 export default function SignupPage(props: PageProps) {
-  const errorMessage = props.url.searchParams.get("error");
+	const errorMessage = props.url.searchParams.get('error');
 
-  return (
-    <>
-      <Head title="Signup" />
-      <div class="max-w-xs flex h-screen m-auto">
-        <div class="m-auto w-72">
-          <a href="/">
-            <Logo class="mb-8" />
-          </a>
-          {errorMessage === "User already registered" && (
-            <Notice>{errorMessage}</Notice>
-          )}
-          <AuthForm type="Signup" />
-          <hr class="my-4" />
-          <OAuthLoginButton provider="github">
-            <GitHub class="inline mr-2 h-5 w-5 align-text-top" />{" "}
-            Login with GitHub
-          </OAuthLoginButton>
-          <div class="text-center text-gray-500 hover:text-black mt-8">
-            <a href="/login">Already have an account? Log in</a>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head title="Signup" />
+			<div class="max-w-xs flex h-screen m-auto">
+				<div class="m-auto w-72">
+					<a href="/">
+						<Logo class="mb-8" />
+					</a>
+					{errorMessage === 'User already registered' && (
+						<Notice>{errorMessage}</Notice>
+					)}
+					<AuthForm type="Signup" />
+					<hr class="my-4" />
+					<OAuthLoginButton provider="github">
+						<GitHub class="inline mr-2 h-5 w-5 align-text-top" /> Login with
+						GitHub
+					</OAuthLoginButton>
+					<div class="text-center text-gray-500 hover:text-black mt-8">
+						<a href="/login">Already have an account? Log in</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
